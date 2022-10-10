5bcfbe791073930b2d38f7cadb639ffc2c140dc0
Instet <Instet163@mail.ru>
2022-10-10 19:19:09 +0400

    error

---
+++ Navigation/NavigationTests/LogInViewControllerTest.swift
@@ -0,0 +1,25 @@
+//
+//  LogInViewControllerTest.swift
+//  NavigationTests
+//
+//  Created by Руслан Магомедов on 10.10.2022.
+//
+
+import XCTest
+@testable import Navigation
+
+final class LogInViewControllerTest: XCTestCase {
+
+
+    override func setUpWithError() throws {
+    }
+
+    func test_pressLogIn() {
+
+    }
+
+    func test_registerAction() {
+
+    }
+    
+}
